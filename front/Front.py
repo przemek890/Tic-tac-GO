38a5ca3545e47650c7a1e1b0eb8a381edbf6a2f6
przemek890 <99132132+przemek890@users.noreply.github.com>
2023-02-17 16:43:45 +0100

    Add files via upload

--- front/Front.py
+++ front/Front.py
@@ -1,15 +1,20 @@
 import pygame
+import os
 
 class Frontend:
     def __init__(self,screen):
-        self.background = pygame.image.load('./data/tlo.png')
-        self.board = pygame.image.load('./data/board.png')
-        self.assets = pygame.image.load('./data/assets.png')
-        self.o = pygame.image.load('./data/o.png')
-        self.x = pygame.image.load('./data/x.png')
-        self.draw = pygame.image.load("./data/draw.png")
-        self.cross = pygame.image.load('./data/cross.png')
-        self.circle = pygame.image.load('./data/circle.png')
+        if os.name == "posix":
+            path = "./"
+        elif os.name == "nt":
+            path = "../"
+        self.background = pygame.image.load('{}data/tlo.png'.format(path))
+        self.board = pygame.image.load('{}data/board.png'.format(path))
+        self.assets = pygame.image.load('{}data/assets.png'.format(path))
+        self.o = pygame.image.load('{}data/o.png'.format(path))
+        self.x = pygame.image.load('{}data/x.png'.format(path))
+        self.draw = pygame.image.load('{}data/draw.png'.format(path))
+        self.cross = pygame.image.load('{}data/cross.png'.format(path))
+        self.circle = pygame.image.load('{}data/circle.png'.format(path))
         self.screen_height = screen.get_height()
         self.background = pygame.transform.scale(self.background, (self.screen_height,self.screen_height))
         screen.blit(self.background, (0, 0))
